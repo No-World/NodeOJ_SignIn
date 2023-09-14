8014396246c197f5e01c98a6a37aa26af50b051d
NoWorld <2259881867@qq.com>
2023-09-14 17:16:13 +0800

    代码格式修改

--- index.py
+++ index.py
@@ -8,40 +8,39 @@ import urllib.parse
 import urllib.request
 
 def main_handler(event, context):
+    key = os.environ.get('KEY')
+    # key = '你的server酱sendkey'
+    cookies = {'ssid': os.environ.get('COOKIES')}
+    # cookies = {'ssid': '你的nodeoj的cookies'}
 
-  key = os.environ.get('KEY')
-  # key = '你的server酱sendkey'
-  cookies = { 'ssid': os.environ.get('COOKIES') }
-  # cookies = {'ssid': '你的nodeoj的cookies'}
-
-  ret = sc_send('签到结果', signin(cookies), key)
-  print(ret)
-
+    ret = sc_send('签到结果', signin(cookies), key)
+    print(ret)
 
 def signin(cookies):
-  url = 'https://nodeoj.com/qiandao/ouhuang'
+    url = 'https://nodeoj.com/qiandao/ouhuang'
 
-  headers = { 'User-Agent': 'No_World签到姬'.encode('utf-8') }
-  response = requests.get(url, cookies=cookies, headers=headers)
-  response_text = response.text
-  data = json.loads(response_text)
-  status = data['status']
-  text = status.replace('<br/>','\n\n')
+    headers = {'User-Agent': 'No_World签到姬'.encode('utf-8')}
+    response = requests.get(url, cookies=cookies, headers=headers)
+    response_text = response.text
+    data = json.loads(response_text)
+    status = data['status']
+    text = status.replace('<br/>', '\n\n')
 
-  # 获取的时间戳并非签到时间
-  # if 'time' in data:
-  #   dt = datetime.fromisoformat(data['time'][:-1])
-  #   text += '签到时间:' + dt.strftime('%Y-%m-
+    # 获取的时间戳并非签到时间
+    # if 'time' in data:
+    #   dt = datetime.fromisoformat(data['time'][:-1])
+    #   text += '签到时间:' + dt.strftime('%Y-%m-
 
-  return text
+    return text
 
 def sc_send(title, content, key):
-  postdata = urllib.parse.urlencode({'text': title, 'desp': content}).encode('utf-8')
-  url = f'https://sctapi.ftqq.com/{key}.send'
-  req = urllib.request.Request(url, data=postdata, method='POST')
-  with urllib.request.urlopen(req) as response:
-    result = response.read().decode('utf-8')
-  return result
+    postdata = urllib.parse.urlencode({'text': title, 'desp': content}).encode('utf-8')
+    url = f'https://sctapi.ftqq.com/{key}.send'
+    req = urllib.request.Request(url, data=postdata, method='POST')
+    with urllib.request.urlopen(req) as response:
+        result = response.read().decode('utf-8')
+    return result
+
 
 if __name__ == '__main__':
-  main_handler(None, None)
+    main_handler(None, None)
